a27dcaef52747ff2fe753ba26080281d5429efec
Gevorg Vardanyan <135601337+gevorg-vardanyan@users.noreply.github.com>
2025-09-22 00:13:25 +0400

    Updated surfing_keys.js for kino sites

--- surfing_keys.js
+++ surfing_keys.js
@@ -66,6 +66,9 @@ if (window.location.hostname.includes("kino")) {
     api.map("e", "f")
     api.unmap("f")
     console.log("=============\nin kino website")
+    api.mapkey('h', 'Test', function() {
+        console.log("=============\nin kino website")
+    } );
 }
 
 if (window.location.hostname === 'www.youtube.com' || window.location.hostname === 'www.youtu.be') {
