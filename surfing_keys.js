62e2dfe6b372e067c77382ba5f70ade76c9a90ee
Gevorg Vardanyan <135601337+gevorg-vardanyan@users.noreply.github.com>
2025-11-03 20:33:42 +0400

    Updated 'f, m, e' keymaps for kino websites

--- surfing_keys.js
+++ surfing_keys.js
@@ -62,13 +62,6 @@ mapkey('gp', '#4Go to the playing tab', function() {
     })
 }, { repeatIgnore: true });
 
-if (window.location.hostname.includes("kino")) {
-    console.log("==============\nhostname:", window.location.hostname)
-    api.map("e", "f");
-    api.unmap("f", /kinogo.media/);
-    // api.unmap("f", '/' + window.location.hostname + '/');
-}
-
 if (window.location.hostname === 'www.youtube.com' || window.location.hostname === 'www.youtu.be') {
     settings.digitForRepeat = false;
     api.map("e", "f")
@@ -181,6 +174,12 @@ if (window.location.hostname === 'app.dupdub.com') {
     } );
 }
 
+if (window.location.hostname.includes("kino")) {
+    console.log("==============\nhostname:", window.location.hostname)
+    // api.map("e", "f");
+    api.unmap("f");
+    // api.unmap("f", '/' + window.location.hostname + '/');
+}
 
 // only keep E, R and T from Surfingkeys for gmail.com and twitter.com
 // api.unmapAllExcept(['E','R','T'], /gmail.com|twitter.com/);
